6dbb4940c641eb5af88ec4c3456943950d6bd4c5
Sándor Képiró <sandor.kepiro@phmu.de>
2019-12-08 22:30:19 +0100

    add cleanup after listeners

--- src/components/util/HandleDragEvents.vue
+++ src/components/util/HandleDragEvents.vue
@@ -2,60 +2,53 @@
 export default {
   mounted() {
     let dragged
-    document.addEventListener(
-      'dragstart',
-      function(event) {
-        dragged = event.target
-        event.target.classList.add('is-moving')
-      },
-      false
-    )
+    const dragStartHandler = (event) => {
+      dragged = event.target
+      event.target.classList.add('is-moving')
+    }
+    document.addEventListener('dragstart', dragStartHandler, false)
 
-    document.addEventListener(
-      'dragend',
-      function(event) {
-        event.target.classList.remove('is-moving')
-      },
-      false
-    )
+    const dragEndHandler = (event) => {
+      event.target.classList.remove('is-moving')
+    }
+    document.addEventListener('dragend', dragEndHandler, false)
 
-    document.addEventListener(
-      'dragover',
-      function(event) {
-        event.preventDefault()
-      },
-      false
-    )
+    const dragOverHandler = (event) => {
+      event.preventDefault()
+    }
+    document.addEventListener('dragover', dragOverHandler, false)
 
-    document.addEventListener(
-      'drop',
-      (event) => {
-        event.preventDefault()
-        const draggedElement = dragged
-        const container = dragged.parentElement.parentElement
-        const indexOfElement = this.getNodeIndex(draggedElement) - 1
-        const indexOfContainer = this.getNodeIndex(container)
-        const tasksStatusKey = this.getTasksStatusKeyByContainer(
-          indexOfContainer
-        )
+    const dropHandler = (event) => {
+      event.preventDefault()
+      const draggedElement = dragged
+      const container = dragged.parentElement.parentElement
+      const indexOfElement = this.getNodeIndex(draggedElement) - 1
+      const indexOfContainer = this.getNodeIndex(container)
+      const tasksStatusKey = this.getTasksStatusKeyByContainer(indexOfContainer)
 
-        const wrapper = this.getParentById(event.toElement, 'progress')
-        if (!wrapper) return
-        const targetProgress = Number(wrapper.id[wrapper.id.length - 1])
-        if (targetProgress === indexOfContainer) return
-        this.$emit('updateTaskByKey', {
-          taskToUpdate: this.$attrs[tasksStatusKey][indexOfElement],
-          keyValuePair: {
-            key: 'progress',
-            value: {
-              title: this.getTitleOfTaskStatusByValue(targetProgress),
-              value: targetProgress,
-            },
+      const wrapper = this.getParentById(event.toElement, 'progress')
+      if (!wrapper) return
+      const targetProgress = Number(wrapper.id[wrapper.id.length - 1])
+      if (targetProgress === indexOfContainer) return
+      this.$emit('updateTaskByKey', {
+        taskToUpdate: this.$attrs[tasksStatusKey][indexOfElement],
+        keyValuePair: {
+          key: 'progress',
+          value: {
+            title: this.getTitleOfTaskStatusByValue(targetProgress),
+            value: targetProgress,
           },
-        })
-      },
-      false
-    )
+        },
+      })
+    }
+    document.addEventListener('drop', dropHandler, false)
+
+    this.$once('hook:beforeDestroy', () => {
+      document.removeEventListener('dragstart', dragStartHandler, false)
+      document.removeEventListener('dragend', dragEndHandler, false)
+      document.removeEventListener('dragover', dragOverHandler, false)
+      document.removeEventListener('drop', dropHandler, false)
+    })
   },
   methods: {
     getTitleOfTaskStatusByValue(statusValue) {
